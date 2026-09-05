832d034b62832bce896e1c446bc28157465c7baf
NitinKumar1054 <nitin2911.in@gmail.com>
2026-09-05 10:19:55 +0530

    feat(auth): add register/login controllers and auth protection middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('RentEasy API is running...');
